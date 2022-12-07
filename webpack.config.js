137af550342ead17492d3fa987332e163c128c26
Lucas David Erkana <lderkana21x@gmail.com>
2022-12-07 13:18:43 +0200

    Add style.css

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
      path: path.resolve(__dirname, 'dist'),
      clean: true,
    },
+   module: {
+    rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
  };
